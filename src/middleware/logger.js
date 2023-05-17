c6ca7007080f515d0fb3192502adcdc8072497ea
KaedenOC <komeacarr2@gmail.com>
2023-05-17 09:52:06 -0700

    bug fixes

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,7 +1,12 @@
 'use strict';
 
+// const { request } = require('express');
+
 const logger = (req, res, next) => {
-  console.log('logger middleware hit');
+  console.log({
+    method: req.method,
+    path: req.path,
+  });
 
   //next called with no argument means move on to the next middleware
   next();
