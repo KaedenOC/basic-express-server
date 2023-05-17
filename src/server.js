81f3303c6f838678413f463334a0f5f35196b916
KaedenOC <komeacarr2@gmail.com>
2023-05-16 17:06:57 -0700

    add middleware files, logger and validator working properly

--- src/server.js
+++ src/server.js
@@ -3,16 +3,29 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const logger = require('./middleware/logger');
+const validator = require('./middleware/validator');
+
+
 
 //create express singleton
 const app = express();
-// const PORT = process.env.PORT;
 
 //middleware
 app.use(cors());
 app.use(express.json());
+app.use(logger);
 
+//proof of life
+app.get('/', (req, res, next) => {
+  res.status(200).send('proof of life');
+});
 
+//person path
+app.get('/person', validator, (req, res, next) => {
+  console.log(req.query);
+  res.status(200).send(req.query);
+});
 
 
 const start = (port) => app.listen(port, () => console.log('listening on port:', port));
